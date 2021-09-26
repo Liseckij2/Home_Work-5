9d7fe298ae83cac06fad70a4568f3a025c6c8988
Andrew Liseczkiy <45668739+Liseckij2@users.noreply.github.com>
2021-09-27 00:52:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-function thisFoo() {
-let start;
-let addnumber;
-let arr = [];
-let count = 1;
 
-function addInMassiveNumbers(start) {
+    let start;
+    let addNumber;
+    let arr = [];
+    let count = 1;
 
+    function addInMassiveNumbers(start) {
     do {
         start = +prompt('How many elements in massive you want?\nmin 5 simbols\nmax 20 simbols');
-    } while (start < 5 || start > 20 || start !== start);
-    
-do {
-
+        } while (start < 5 || start > 20 || start !== start);
+        
     do {
-    addnumber = +prompt(`Send number for add in massive ${count}`);
-    } while (addnumber !== addnumber);
-
-    count++;
-    start--;
-    arr.push(addnumber);
-
-} while (start >= 1);
-
-};
-
-function SelectionSort(arr)      
-{                             
-    var n = arr.length; 
-    for (var i = 0; i < n-1; i++) 
-     { var min = i; 
-       for (var j = i+1; j < n; j++) 
-        { if (arr[j] < arr[min]) min = j; }  
-       var t = arr[min]; arr[min] = arr[ i ]; arr[ i ] = t; 
-     }                     
-     console.log(arr);   // На выходе сортированный по возрастанию массив . 
-    return alert(`Add massive result: ${arr}`); 
-}
-return addInMassiveNumbers(start), SelectionSort(arr);
-};
-thisFoo();
+    do {
+        addNumber = +prompt(`Send number for add in massive ${count}`);
+    } while (addNumber !== addNumber);
+        count++;
+        start--;
+        arr.push(addNumber);
+    } while (start >= 1);
+    // console.log(arr);
+    return arr;
+    };
+    addInMassiveNumbers(start);
+    
+    function SelectionSort(arr)      
+    {                             
+        var n = arr.length; 
+        for (var i = 0; i < n-1; i++) 
+         { var min = i; 
+           for (var j = i+1; j < n; j++) 
+            { if (arr[j] < arr[min]) min = j; }  
+           var t = arr[min]; arr[min] = arr[ i ]; arr[ i ] = t; 
+         }                     
+         console.log(arr);   // На выходе сортированный по возрастанию массив . 
+         return arr;
+    }
+    SelectionSort(arr);
