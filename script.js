cf305b3edec88f537bba472315818c31567d803b
Andrew Liseczkiy <45668739+Liseckij2@users.noreply.github.com>
2021-10-13 02:43:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
+let arr = []; // избавится от этого не могу, по скольку эта переменная используется в обеих функциях... 
 
-    let start;
-    let addNumber;
-    let arr = [];
-    let count = 1;
-
-    function addInMassiveNumbers(start) {
-    do {
-        start = +prompt('How many elements in massive you want?\nmin 5 simbols\nmax 20 simbols');
-        } while (start < 5 || start > 20 || start !== start);
-        
-    do {
-    do {
-        addNumber = +prompt(`Send number for add in massive ${count}`);
-    } while (addNumber !== addNumber);
-        count++;
-        start--;
-        arr.push(addNumber);
-    } while (start >= 1);
-    // console.log(arr);
+function addInMassiveNumbers() {
+  let start;
+  let count = 1;
+  do {
+      start = +prompt("How many elements in massive you want?\nmin 5 simbols\nmax 20 simbols");
+  } while (start < 5 || start > 20 || start !== start);
+  do {
+  let addNumber;
+  do {
+      addNumber = +prompt(`Send number for add in massive ${count}`);
+  } while (addNumber !== addNumber);
+    count++;
+    start--;
+    arr.push(addNumber);
+  } while (start >= 1);
     return arr;
-    };
-    addInMassiveNumbers(start);
-    
-    function SelectionSort(arr)      
-    {                             
-        var n = arr.length; 
-        for (var i = 0; i < n-1; i++) 
-         { var min = i; 
-           for (var j = i+1; j < n; j++) 
-            { if (arr[j] < arr[min]) min = j; }  
-           var t = arr[min]; arr[min] = arr[ i ]; arr[ i ] = t; 
-         }                     
-         console.log(arr);   // На выходе сортированный по возрастанию массив . 
-         return arr;
+}
+addInMassiveNumbers();
+
+function SelectionSort() {
+  var n = arr.length;
+  for (var i = 0; i < n - 1; i++) {
+  var min = i;
+  for (var j = i + 1; j < n; j++) {
+   if (arr[j] < arr[min]) min = j;
     }
-    SelectionSort(arr);
+  var t = arr[min];
+    arr[min] = arr[i];
+    arr[i] = t;
+  }
+    console.log(arr);
+    return arr;
+}
+SelectionSort();
